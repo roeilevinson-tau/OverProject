6eefa3936aaaed7b3ea11aa9c2c549129387f239
Roei <roeil@mail.tau.ac.il>
2024-11-15 22:35:39 +0200

    9

--- Prev_final_100/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import Extension, setup
-
-module = Extension("symnmfmodule", sources=['symnmfmodule.c', 'symnmf.c'])
-setup(name='symnmfmodule',
-      version='1.0',
-      description='Python wrapper for custom C extension',
-      ext_modules=[module])
